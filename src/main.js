8656e6083cd5c4c406be265980a03e654fb612e5
Olga Kozlova <olgakozlova@coherentsolutions.com>
2017-09-04 10:53:17 +0300

    Redirect to calendar added

--- src/main.js
+++ src/main.js
@@ -2,20 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import { Route } from 'react-router';
+import { Route, Redirect, Switch } from 'react-router';
 import { ConnectedRouter, routerReducer, routerMiddleware } from 'react-router-redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import createHistory from 'history/createBrowserHistory';
 import * as reducers from './reducers';
 import CalendarView from './Components/views/CalendarView/CalendarView.jsx';
-// Create a history of your choosing (we're using a browser history in this case)
+
 const history = createHistory();
-
-// Build the middleware for intercepting and dispatching navigation actions
 const rMiddleware = routerMiddleware(history);
-
-
 const store = createStore(
     combineReducers(
         {
@@ -31,8 +27,11 @@ ReactDOM.render(
     <Provider store={store}>
         <ConnectedRouter history={history}>
             <div>
-                <Route exact path="/calendar" component={CalendarView} />
-                <Route path="/" component={CalendarView} />
+                <Switch>
+                    <Route exact path="/calendar" component={CalendarView} />
+                    <Route exact path="/news" component={CalendarView} />
+                    <Redirect from="/" exact to="/calendar" />
+                </Switch>
             </div>
         </ConnectedRouter>
     </Provider>,
